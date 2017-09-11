f52c1b359495a8617cf4657462cdd517115e0977
Sławomir Jopek <pwnsoon@gmail.com>
2017-09-11 09:55:45 +0200

    initial

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var del = require('del');
+var minifyJs = require('gulp-minify');
+var minyfyCSS = require('gulp-cssnano');
+
+gulp.task('clean', function() {
+    del('dist/');
+});
+
+gulp.task('styles', function() {
+    gulp.src('src/preloader.scss')
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('dist/'))
+        .pipe(minyfyCSS())
+        .pipe(gulp.dest('dist/min/'))
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('src/preloader.js')
+        .pipe(gulp.dest('dist/'))
+        .pipe(minifyJs({
+            noSource: true
+        }))
+        .pipe(gulp.dest('dist/min/'))
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/preloader.scss', ['styles']);
+    gulp.watch('src/preloader.js', ['scripts'])
+});
